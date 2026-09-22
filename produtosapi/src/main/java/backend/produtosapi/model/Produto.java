dad567d07e0a04addf64f633aed40496a3643a0b
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-09-22 15:53:09 -0300

    feat: setup inicial da API de Produtos
    - Criação da estrutura base do projeto com Spring Boot e Maven
    - Implementação da ProdutosapiApplication
    - Criação dos primeiros endpoints no ProdutoController
    - Configuração de conexão com o banco de dados (application.yml)
    - Adição do script data.sql para criação da tabela de produtos

---
+++ produtosapi/src/main/java/backend/produtosapi/model/Produto.java
@@ -0,0 +1,68 @@
+package backend.produtosapi.model;
+
+public class Produto {
+
+    private String id, nome, descricao;
+    private double preco;
+
+    public String getId() {
+
+        return id;
+
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+
+    }
+
+    public double getPreco() {
+
+        return preco;
+
+    }
+
+    public void setPreco(double preco) {
+
+        this.preco = preco;
+
+    }
+
+    public String getDescricao() {
+
+        return descricao;
+
+    }
+
+    public void setDescricao(String descricao) {
+
+        this.descricao = descricao;
+
+    }
+
+    public String getNome() {
+
+        return nome;
+
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+
+    }
+
+    @Override
+    public String toString() {
+
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+
+    }
+
+}
