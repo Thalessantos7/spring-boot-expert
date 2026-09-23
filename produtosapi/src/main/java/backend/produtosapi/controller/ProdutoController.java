bc266c47105b19dedd9d8e9f254cf22580aedfb6
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-09-23 15:38:35 -0300

    feat: adiciona atualização de dados e buscas customizadas
    - Implementação da funcionalidade para atualizar os dados de um produto existente.
    - Adição de métodos no repositório para realizar buscas de produtos com passagem de parâmetros.

--- produtosapi/src/main/java/backend/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/backend/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import backend.produtosapi.model.Produto;
 import backend.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -43,6 +44,21 @@ public class ProdutoController {
     public void deletar(@PathVariable("id") String id) {
 
         produtoRepository.deleteById(id);
+
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
+
+        produto.setId(id);
+        produtoRepository.save(produto);
+
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+
+        return produtoRepository.findByNome(nome);
 
     }
 
--- produtosapi/src/main/java/backend/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/backend/produtosapi/repository/ProdutoRepository.java
@@ -3,4 +3,10 @@ package backend.produtosapi.repository;
 import backend.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository<Produto, String> {}
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+
+}
