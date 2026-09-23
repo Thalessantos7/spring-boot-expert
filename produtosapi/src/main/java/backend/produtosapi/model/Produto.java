739d9bfd7917e60bea2ca8336256006d509e8d1a
Thales Santos <thalesferreiramontenegro@gmail.com>
2026-09-23 06:41:06 -0300

    feat: implementa mapeamento JPA e operações de persistência da entidade Produto
    - Criação do mapeamento JPA para a entidade Produto.
    - Implementação da funcionalidade para persistir produtos no banco de dados.
    - Adição de métodos para obter os dados de produtos salvos.
    - Implementação da funcionalidade de deleção de produtos.

--- produtosapi/src/main/java/backend/produtosapi/model/Produto.java
+++ produtosapi/src/main/java/backend/produtosapi/model/Produto.java
@@ -1,8 +1,25 @@
 package backend.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 
-    private String id, nome, descricao;
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco")
     private double preco;
 
     public String getId() {
